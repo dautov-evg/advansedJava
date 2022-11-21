d26420204b497949953f9a74222de080b2b545e6
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-21 03:09:24 +0300

    lesson 25 end

---
+++ src/Multithreading_03/ProducerConsumer25/Test1.java
@@ -0,0 +1,49 @@
+package Multithreading_03.ProducerConsumer25;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test1 {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 3; i++)
+            executorService.submit(new Processor1(i, countDownLatch));
+
+        executorService.shutdown();
+
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(500);
+            countDownLatch.countDown();
+        }
+
+    }
+}
+
+class Processor1 implements Runnable {
+    private int id;
+    private CountDownLatch countDownLatch;
+
+    public Processor1(int id, CountDownLatch countDownLatch) {
+        this.id = id;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Thread with id " + id + " proceeded.");
+    }
+}
