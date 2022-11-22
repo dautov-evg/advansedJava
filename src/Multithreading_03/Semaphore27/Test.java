2168d3a1df4d0c27f09d674ee5245364651f7063
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-22 19:15:03 +0300

    lesson 27 end

---
+++ src/Multithreading_03/Semaphore27/Test.java
@@ -0,0 +1,65 @@
+package Multithreading_03.Semaphore27;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
+
+        Connection connection = Connection.getConnection();
+        for (int i = 0; i < 200; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        connection.work();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
+
+//Singleton
+class Connection {
+    private static Connection connection = new Connection();
+    private int conectionsCount;
+    private Semaphore semaphore = new Semaphore(10);
+
+    private Connection() {
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public void work() throws InterruptedException {
+        semaphore.acquire();
+        try {
+            doWork(); // делаем так, чтобы если выпадает исключение,
+            //всегда вызывался метод release();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void doWork() throws InterruptedException {
+        synchronized (this) {
+            conectionsCount++;
+            System.out.println(conectionsCount);
+        }
+        Thread.sleep(5000);
+
+        synchronized (this) {
+            conectionsCount--;
+        }
+    }
+}
