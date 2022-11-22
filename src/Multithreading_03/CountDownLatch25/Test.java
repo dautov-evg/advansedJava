bca3ca82c05b383ee017969df3bcbae938bfb37b
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-22 03:44:20 +0300

    lesson 24 end

--- src/Multithreading_03/CountDownLatch25/Test.java
+++ src/Multithreading_03/CountDownLatch25/Test.java
@@ -1,49 +1,39 @@
-package Multithreading_03.ProducerConsumer25;
+package Multithreading_03.CountDownLatch25;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Test1 {
+public class Test {
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(3);
 
         ExecutorService executorService = Executors.newFixedThreadPool(3);
 
         for (int i = 0; i < 3; i++)
-            executorService.submit(new Processor1(i, countDownLatch));
+            executorService.submit(new Processor(countDownLatch));
 
         executorService.shutdown();
 
-        for (int i = 0; i < 3; i++) {
-            Thread.sleep(500);
-            countDownLatch.countDown();
-        }
-
+        countDownLatch.await();
+        System.out.println("Latch has been opened, main thread is proceeding!");
     }
 }
 
-class Processor1 implements Runnable {
-    private int id;
+class Processor implements Runnable {
     private CountDownLatch countDownLatch;
 
-    public Processor1(int id, CountDownLatch countDownLatch) {
-        this.id = id;
+    public Processor(CountDownLatch countDownLatch) {
         this.countDownLatch = countDownLatch;
     }
 
     @Override
     public void run() {
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
         try {
-            countDownLatch.await();
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Thread with id " + id + " proceeded.");
+        countDownLatch.countDown();
     }
 }
