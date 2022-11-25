f67c0d4527e6c675d79ab958a8b7854c47f4ee73
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-25 21:19:46 +0300

    lesson 31 end

--- src/Regexp_04/Lesson_31/Test.java
+++ src/Regexp_04/Lesson_31/Test.java
@@ -1,4 +1,21 @@
 package Regexp_04.Lesson_31;
 
+import java.util.Arrays;
+
 public class Test {
+    public static void main(String[] args) {
+        String a = "Hello1322154there132135651hey31351813quqewre";
+        String[] words = a.split("\\d+");
+        //System.out.println(Arrays.toString(words));
+
+        String b = "Hello there here";
+        String modifyString = b.replace(" ", ".");
+        //System.out.println(modifyString);
+
+        String c = "Hello135651there6151618here";
+        String modifyStringAll = c.replaceAll("\\d+", " ");
+        System.out.println(modifyStringAll);
+        String modify = c.replaceFirst("\\d+", " ");
+        System.out.println(modify);
+    }
 }
